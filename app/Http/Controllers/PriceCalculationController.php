b5938ac66ae559d3dcc4a1b2d88c106dcfb67889
nao.u <nao.study0@gmail.com>
2021-07-20 22:27:19 +0900

    [update]バリデーションによるリダイレクト時にエラーメッセージを表示

--- app/Http/Controllers/PriceCalculationController.php
+++ app/Http/Controllers/PriceCalculationController.php
@@ -4,7 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
-use App\PriceCalculation\CalcPrice;
+use App\Models\Price;
+use App\Http\Requests\InputPriceDataRequest;
 
 class PriceCalculationController extends Controller
 {
@@ -21,20 +22,8 @@ class PriceCalculationController extends Controller
      * 料金計算結果を表示する
      * 
      */
-    public function showResult(Request $request)
+    public function showResult(InputPriceDataRequest $request)
     {
-        // バリデーション
-        $validator = Validator::make($request->all(),[
-            'enter-time' => 'required | date',
-            'leave-time' => 'required | date | after_or_equal:enter-time',
-            'course' => 'required | numeric | regex:/^[0-3]+$/',
-        ]);
-
-        // 誤りがある場合はリダイレクト
-        if($validator->fails()) {
-            return redirect('/');
-        }
-
         // 入店日時のDateTimeImmutableクラス作成
         $enter_time = $request['enter-time'];
         $enter_datetime_immutable = new \DateTimeImmutable($enter_time);
@@ -46,19 +35,15 @@ class PriceCalculationController extends Controller
         // コースの種別
         $course_number = $request['course'];
 
-
         // 料金計算クラスをインスタンス化
-        $calc_price = new CalcPrice($course_number);
-        
+        $calc_price = new Price($course_number);
         // 料金を取得
         $price = $calc_price->calcPrice($enter_datetime_immutable, $leave_datetime_immutable);
        
         // 画面表示用のデータを作成
         $view_data = $calc_price->calcViewTime();
-
         // 滞在時間（画面表示用）
         $stay_time = $enter_datetime_immutable->diff($leave_datetime_immutable); 
-
 
         return view('result', [
             'enter_datetime_immutable' => $enter_datetime_immutable,
--- resources/views/input.blade.php
+++ resources/views/input.blade.php
@@ -10,11 +10,18 @@
     <form method="POST" action="{{ route('price_calculation.result') }}">
         @csrf
         <p><label>入店日時<input type="datetime-local" step="1" name="enter-time"></label></p>
+        @if ($errors->has('enter-time'))
+        <p class="caution">{{$errors->first('enter-time')}}</p>
+        @endif
+        
         <p><label>退店日時<input type="datetime-local" step="1" name="leave-time"></label><p>
+        @if ($errors->has('leave-time'))
+        <p class="caution">{{$errors->first('leave-time')}}</p>
+        @endif
+
         <p>
             <label>コースの種類
                 <select name="course">
-                    <option>選択してください</option>
                     <option value="0">通常料金（入室から1時間）</option>
                     <option value="1">3時間パック（入室から3時間）</option>
                     <option value="2">5時間パック（入室から5時間）</option>
@@ -22,6 +29,9 @@
                 </select>
             </label>
         </p>
+        @if ($errors->has('course'))
+        <p class="caution">{{$errors->first('course')}}</p>
+        @endif
 
         <button type="submit">送信</button>
     </form>
